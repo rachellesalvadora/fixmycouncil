75d2405e5413c7871f39dd48fc95acbffe32d5d5
Rachelle Salvadora <rachellesalvadora@gmail.com>
2017-05-15 22:10:30 +1000

    Routes Update
    - Added council step 1-3 + submission routes
    - Added report routes + council

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home.page');
+Route::get('/council', 'CouncilController@index')->name('council.page');
+Route::get('/council/{id}', 'CouncilController@showCouncil')->name('council.info');
+
+Route::get('/report/step1', 'ReportController@step1')->name('report.step1');
+Route::post('/report/step2', 'ReportController@step2')->name('report.step2');
+Route::post('/report/step3', 'ReportController@step3')->name('report.step3');
+Route::post('/report/submit', 'ReportController@submit')->name('report.submit');
+Route::get('/report/error', 'ReportController@error')->name('report.error');
+Route::get('/report/success', 'ReportController@success')->name('report.success');
+
+Route::get('/problems', 'ProblemController@index')->name('view.problem');
+Route::get('/problems/{suburb}', 'ProblemController@view')->name('view.problem.suburb');
+
+Route::get('/about', 'AboutController@index')->name('about.page');
+Route::get('/faq', 'FaqController@index')->name('faq.page');
+Route::get('/contact', 'ContactController@index')->name('contact.page');
